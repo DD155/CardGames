dea15f7dc253f6ff9a30c6641fae25b3ad7c5089
Dan <dd155@protonmail.com>
2020-04-20 16:32:54 -0400

    added more comments, changed logic in loadImage function

--- script.js
+++ script.js
@@ -36,21 +36,22 @@ function dealHand(deck, amount) {
     return arr;
 }
 
+//Postcondition: display image corresponding to the current card. "show" parameter checks is card is 
+//face up or not
 function loadImage(hand, index, id, show) {
-    var imageElement = new Image(100, 125);
+    var imageElement = new Image(100, 125); 
     var imageLocation = "/CodingProjects/Web/Cards/resources/CardImages/";
-    if (show)
-        imageLocation += hand[index][0].toString() + hand[index][1] + ".png"; //new file location
-    else imageLocation += "back.png";
+    //change string depending on if the current card should be shown or not
+    imageLocation += show ? hand[index][0].toString() + hand[index][1] + ".png" : "back.png"; 
     imageElement.src = imageLocation;
     imageElement.classList.add("cardImages"); //add class to use css on
     document.getElementById(id).appendChild(imageElement);
 }
 
+//Postcondition: loads all the images of the cards on the hand
 function loadHandImages(hand, id, show) {
-    for (var i = 0; i < hand.length; i++) {
+    for (var i = 0; i < hand.length; i++) 
         loadImage(hand, i, id, show);
-    }
 }
 
 //Precondition: deck is not empty
@@ -58,53 +59,63 @@ function loadHandImages(hand, id, show) {
 function deal(deck, hand, id, show) {
     if (deck.length < 1) return;
 
-    if (blackjackCalculateValue(hand) < 21) {
+    if (blackjackCalculateValue(hand) < 21) { //check if you can get another card
         hand.push(deck.pop());
         loadImage(hand, hand.length - 1, id, show);
-        if (blackjackCalculateValue(hand) > 21) { alert("Game Over"); }
+        if (blackjackCalculateValue(hand) > 21)  alert("Game Over"); 
         if (blackjackCalculateValue(hand) == 21) alert("You have 21!");
     }
 }
 
-
 //Precondition: hand size must be greater than 0.
 function blackjackCalculateValue(hand) {
-    if (hand.length < 0) return;
+    if (hand.length < 1) return; //precondition
     var sum = 0;
     var aces = 0;
 
-    for (var i = 0; i < hand.length; i++) {
-        if (hand[i][0] == 'J' || hand[i][0] == 'Q' || hand[i][0] == 'K')
+    for (var i = 0; i < hand.length; i++) { //go through array to sum up values
+        if (hand[i][0] == 'J' || hand[i][0] == 'Q' || hand[i][0] == 'K') //face cards are worth 10 points
             sum += 10;
-        else if (hand[i][0] == 'A') {
+        else if (hand[i][0] == 'A') {//count ace as 11 for now
             sum += 11; aces++;
         } else sum += hand[i][0];
     }
 
-    while (sum > 21 && aces > 0) {
+    while (sum > 21 && aces > 0) { //if the sum has exceeded 21 due to aces, count them as 1 now.
         sum -= 10;
         aces--;
     }
 
     return sum;
 }
+
+// TODO
+// Precondition: deck is not empty, hand is not empty
+// Postcondition: the AI has chosen to either hit or stay
+// algorithm will be as follows:
+// 1. calculate the sum of hand
+// 2. based on sum, calculate remaining difference to 21. 
+// 3. if difference >= 11, ALWAYS hit
+// 4. else determine probability of getting the difference 
+// e.g hand sum is 12, you can get 9 at most, which is 9/13 (2-9, ace)
+// use this new probability to determine if AI will hit or not.
 function blackjackAI(deck, hand) {
+    if (deck < 1 || hand < 1) return;
 }
 
 function driverBlackjack() {
-    //setTimeout(() => { document.getElementById("actionText").innerHTML = 'Welcome to the game! Currently dealing...'; }, 750);
-    var testDeck = createDeck();
-    shuffle(testDeck);
+    var deck = createDeck();
+    shuffle(deck);
 
     //create hands for the players
-    var hand1 = dealHand(testDeck, 2);
-    var hand2 = dealHand(testDeck, 2);
-    var hand3 = dealHand(testDeck, 2);
-    var player = dealHand(testDeck, 2);
+    var hand1 = dealHand(deck, 2);
+    var hand2 = dealHand(deck, 2);
+    var hand3 = dealHand(deck, 2);
+    var player = dealHand(deck, 2);
 
     //assign onclick event to the Hit button
     document.getElementById('dealButton').onclick = function () {
-        deal(testDeck, player, 'playerHand', true);
+        deal(deck, player, 'playerHand', true);
     }
 
     //show images as they are being dealed
